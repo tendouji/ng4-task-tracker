2056094816bde171efeb0fdc1e1b46701252057d
Tendouji <tendoujir@gmail.com>
2018-01-14 00:51:50 +0800

    changing environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDu5q6RPZOEB6strhLzDBLvJy36FAa30Y4",
+    authDomain: "ng4-task-tracker.firebaseapp.com",
+    databaseURL: "https://ng4-task-tracker.firebaseio.com",
+    projectId: "ng4-task-tracker",
+    storageBucket: "ng4-task-tracker.appspot.com",
+    messagingSenderId: "311151891896"
+  }
 };
